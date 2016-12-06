94b4542c34dba2ec34e0e517f339489ec12dd3ce
Sharoon Thomas <sharoon.thomas@fulfil.io>
2016-12-06 07:30:11 -0500

    Minimal initial package

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="python-walmart",
+    version="0.0.1",
+    url="https://github.com/fulfilio/python-walmart",
+
+    author="Fulfil.IO Inc.",
+    author_email="hello@fulfil.io",
+
+    description="Walmart Marketplace API",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
---
+++ walmart/__init__.py
@@ -0,0 +1,5 @@
+"""python-walmart - Walmart Marketplace API"""
+
+__version__ = '0.0.1'
+__author__ = 'Fulfil.IO Inc. <hello@fulfil.io>'
+__all__ = []
