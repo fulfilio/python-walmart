458e4d553358ea24df01daaf378ae9ec9dab14ad
Tarun Bhardwaj <tarun.bhardwaj@fulfil.io>
2019-07-26 11:51:51 +0530

    Released version 0.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ test_requirements = [
 
 setuptools.setup(
     name="python-walmart",
-    version="0.1.1",
+    version="0.1.2",
     url="https://github.com/fulfilio/python-walmart",
 
     author="Fulfil.IO Inc.",
